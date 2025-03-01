86b3504f9398908e27185c27b574cfb4033b3372
Jayden Gardner <120685791+jayden-gardner@users.noreply.github.com>
2025-02-28 20:37:47 -0700

    Update Everything.R

--- Everything.R
+++ Everything.R
@@ -50,6 +50,8 @@ d42<-read.csv("ManeuverBranchId42.csv")
 d43<-read.csv("ManeuverBranchId43.csv")
 d44<-read.csv("ManeuverBranchId44.csv")
 d45<-read.csv("ManeuverBranchId45.csv")
+
+data_set<-rbind.fill(d1,d2,d3,d4,d5,d6,d7,d8,d9,d10,d11,d12,d13,d14,d15,d16,d17,d18,d19,d20,d21,d22,d23,d24,d25,d26,d27,d28,d29,d30,d31,d32,d33,d34,d35,d36,d37,d38,d39,d40,d41,d42,d43,d44,d45,d46,d47)
 d46<-read.csv("ManeuverBranchId46.csv")
 d47<-read.csv("ManeuverBranchId47.csv")
 
